eada98a863952007f5a62b24a09ec974daaff8b2
NIKHIL NEGI <neginikhilsingh6@gmail.com>
2024-03-10 16:09:59 +0530

    Add new routes and controllers for user functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,26 @@
 import { Router } from "express";
-import {logoutUser, loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    logoutUser,
+     loginUser,
+      registerUser,
+       refreshAccessToken,
+        changeCurrentPassword,
+         getCurrentUser,
+          updateAccountDetails,
+           updateUserAvatar,
+            updateUserCoverImage,
+             getUserChannelProfile,
+              getWatchHistory 
+            } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
 
-router.route("/register").post(
+router
+.route("/register")
+.post(
     upload.fields([
         {
             name: "avatar",
@@ -19,9 +33,47 @@ router.route("/register").post(
     ]), 
     registerUser
     )
-router.route("/login").post(loginUser);
+
+router
+.route("/login")
+.post(loginUser);
+
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
+
+router
+.route("/logout")
+.post(verifyJWT, logoutUser);
+
+router
+.route("/refresh-token")
+.post(refreshAccessToken);
+
+router
+.route("/change-password")
+.post(verifyJWT,changeCurrentPassword);
+
+router
+.route("/current-user")
+.post(verifyJWT,getCurrentUser);
+
+router
+.route("/update-account")
+.patch(verifyJWT,updateAccountDetails);
+
+router
+.route("/update-avatar")
+.patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
+
+router
+.route("/update-cover-image")
+.patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
+
+router
+.route("/c/:username")
+.get(verifyJWT,getUserChannelProfile)
+
+router
+.route("/watch-history")
+.get(verifyJWT,getWatchHistory)
 
 export default router  
